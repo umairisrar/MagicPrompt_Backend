0f7170e3770239102fc06875662074fbea9e3903
umairisrar <umair_israr92@hotmail.com>
2023-11-23 21:09:34 +0500

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,10 +5,14 @@ import {
   getDoc,
   getDocs,
   setDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { auth, firestore } from "../config/firbase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { couponList } from "../coupons/coupons.js";
+import nodemailer from "nodemailer";
+import { emailTemplate } from "../routes/emailTemplate.js";
+
 // import { addDoc } from "firebase/firestore";
 
 export const createUser = async (req, res) => {
@@ -20,7 +24,7 @@ export const createUser = async (req, res) => {
     let name = values.Name;
     let coupon = values.Coupon;
     let password = values.Password;
-
+    await sendEmailtoUser(email, password, coupon, name);
     if (
       !email ||
       email.trim === "" ||
@@ -35,14 +39,11 @@ export const createUser = async (req, res) => {
     }
 
     if (coupon.includes("T1")) {
-      console.log("t1");
       planType = "T1";
     } else if (coupon.includes("T2")) {
       planType = "T2";
-      console.log("t2");
     } else if (coupon.includes("T3")) {
       planType = "T3";
-      console.log("t3");
     }
 
     let docRef;
@@ -61,46 +62,135 @@ export const createUser = async (req, res) => {
       console.log("Coupon is not valid");
       return res.status(500).send({ message: "Coupon is not valid" });
     }
-    console.log("ok3");
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    console.log(userCredential);
-    if (!userCredential) {
-      console.log("Email is already used");
-      return res.status(500).send({ message: "Email is already used" });
-    }
-    const postsCollectionRef = collection(firestore, "users");
-    let addUser = await addDoc(postsCollectionRef, {
-      email,
-      coupon,
-      uuids: [],
-      History: [],
-      paid: true,
-      createdDate: Date.now(),
-      planType,
-      userInfo: { name, paymentStatus: "", profession: "" },
-    });
-    if (!addUser) {
-      console.log("Something went wrong");
-      return res.status(500).send({ message: "Something went wrong" });
-    }
-    if (allCoupons.includes(coupon)) {
-      if (docSnapshot.exists()) {
-        const data = docSnapshot.data();
-        await setDoc(docRef, { coupons: [...data.coupons, coupon] });
-      } else {
-        const initialData = [coupon];
-        await setDoc(docRef, { coupons: initialData });
+    let existUser = false;
+    let user_paid = false;
+    let singleUserid;
+    let users = await getDocs(collection(firestore, "users"));
+    users.forEach(async (singleUser) => {
+      let userData = singleUser.data();
+      if (userData.email === email) {
+        existUser = true;
+        singleUserid = singleUser.id;
       }
+    });
+    if (existUser) {
+      if (!user_paid) {
+        let document = doc(firestore, "users", singleUserid);
+        await updateDoc(document, {
+          plan: true,
+          userInfo: {
+            name: name,
+          },
+          coupon,
+          planType,
+        });
+        if (allCoupons.includes(coupon)) {
+          if (docSnapshot.exists()) {
+            const data = docSnapshot.data();
+            await setDoc(docRef, { coupons: [...data.coupons, coupon] });
+          } else {
+            const initialData = [coupon];
+            await setDoc(docRef, { coupons: initialData });
+          }
+        }
+        let passwordvalue = "************";
+        await sendEmailtoUser(email, passwordvalue, coupon, name);
+        return res
+          .status(200)
+          .send({ message: "New User Updated Successfully" });
+      } else {
+        return res.status(500).send({ message: "you have already paid" });
+      }
+    } else {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (!userCredential) {
+        console.log("Email is already in used");
+        return res.status(500).send({ message: "Email is already used" });
+      }
+      const postsCollectionRef = collection(firestore, "users");
+      let addUser = await addDoc(postsCollectionRef, {
+        email,
+        plan: true,
+        coupon,
+        uuids: [],
+        History: [],
+        paid: true,
+        createdDate: Date.now(),
+        planType,
+        userInfo: { name, paymentStatus: "", profession: "" },
+      });
+      if (!addUser) {
+        console.log("Something went wrong");
+        return res.status(500).send({ message: "Something went wrong" });
+      }
+      if (allCoupons.includes(coupon)) {
+        if (docSnapshot.exists()) {
+          const data = docSnapshot.data();
+          await setDoc(docRef, { coupons: [...data.coupons, coupon] });
+          await sendEmailtoUser(email, password, coupon, name);
+        } else {
+          const initialData = [coupon];
+          await setDoc(docRef, { coupons: initialData });
+          await sendEmailtoUser(email, password, coupon, name);
+        }
+      }
+      console.log("New User Created Successfully");
+      return res.status(200).send({ message: "New User Created Successfully" });
     }
-    console.log("New User Created Successfully");
-    return res.status(200).send({ message: "New User Created Successfully" });
   } catch (error) {
     console.log({ error: error.message });
     return res.status(500).send({ message: error.message });
+  }
+};
+
+const sendEmailtoUser = async (email, password, coupon, name) => {
+  let supportemail = process.env.SUPPORT_EMAIL;
+  let senderemail = process.env.SENDER_EMAIL;
+  let founder = process.env.FOUNDER;
+  let SENDER_PASSWORD = process.env.SENDER_PASSWORD;
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "mail.promptsgenii.com",
+      port: 26,
+      secure: false,
+      auth: {
+        user: senderemail,
+        pass: SENDER_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+    let mailOptions = {
+      from: senderemail,
+      to: email,
+      subject: "Your Account Information",
+      html: emailTemplate(
+        email,
+        password,
+        name,
+        supportemail,
+        senderemail,
+        founder
+      ),
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        // response.send(error);
+      } else {
+        // response.send("Message sent: %s", info.messageId);
+        console.log("Message sent: %s", info.messageId);
+      }
+    });
+  } catch (err) {
+    // response.send(err);
+    console.log(err);
+    console.log({ message: "Not Sending Email" });
   }
 };
 
